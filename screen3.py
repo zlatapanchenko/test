acb575224c8bb26cb521286e790343b05338e15a
zlatapancheko <panchenkoza2008@mail.ru>
2023-07-15 16:42:37 +0300

    сам файл

---
+++ screen3.py
@@ -0,0 +1,9 @@
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+class string(QWidget):
+    def __init__(self):
+        super().__init__()
+        pass
